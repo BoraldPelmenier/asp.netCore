f4f5619818dbe889b2d22bc455e10a313d0b24fd
BoraldPelmenier@gmail.com <58140350+BoraldPelmenier@users.noreply.github.com>
2021-07-13 23:56:38 +0700

    Добавьте файлы проекта.

---
+++ WebEducation/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEducation.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
